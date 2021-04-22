ca8cb5c65c376090e462d8396f639f5e6889f35e
Lau Jørgensen <laux0305@student.eadania.dk>
2021-04-22 11:50:51 +0200

    bait database

---
+++ DataBros/Bait.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBros
+{
+    public class Bait
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+
+        public string BaitName { get; set; }
+    }
+}
