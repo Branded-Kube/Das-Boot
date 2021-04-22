79ae3547ea40d0b23feab6023695b71a8c16830b
rasbrandt <mrkoldbrandt@gmail.com>
2021-04-22 21:22:20 +0200

    database tables

--- DataBros/Repository.cs
+++ DataBros/Repository.cs
@@ -22,6 +22,21 @@ namespace DataBros
         {
             var cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS characters (Id INTEGER PRIMARY KEY, Name VARCHAR(50), Experience INTEGER);", (SQLiteConnection)connection);
             cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS Players (ID INTEGER PRIMARY KEY, Name VARCHAR(50), Money INTEGER, Strengt INTEGER, FOREIGN KEY (ID) REFERENCES Region(ID));", (SQLiteConnection)connection);
+            cmd.ExecuteNonQuery();
+            
+            cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS Fish (ID INTEGER PRIMARY KEY, Name VARCHAR(50), BiteTime INTEGER, Strengt INTEGER, Weight INTEGER, Price INTEGER);", (SQLiteConnection)connection);
+            cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS Region (ID INTEGER PRIMARY KEY, Name VARCHAR(50), FOREIGN KEY (ID) REFERENCES Water(ID));", (SQLiteConnection)connection);
+            cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS Water (ID INTEGER PRIMARY KEY, Name VARCHAR(50), Size INTEGER, Type BOOLEAN, FOREIGN KEY (ID) REFERENCES Fish(ID));", (SQLiteConnection)connection);
+            cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS Bait (ID INTEGER PRIMARY KEY, BiteTimeMultiplier INTEGER, Price INTEGER, Name VARCHAR(50), Type BOOLEAN);", (SQLiteConnection)connection);
+            cmd.ExecuteNonQuery();
         }
 
         public void AddCharacter(string name, int experience)
